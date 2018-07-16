c15046ea25328ea4bbafbab1412fb311563aee3a
jarretbryan <jarret.bryan@gmail.com>
2018-07-16 08:49:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(array, name) {
+  return array.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(array, partialName) {
+  let nameLength = partialName.length;
+  return array.filter(function (driverName) {
+    return driverName.slice(0, nameLength) === partialName;
+  });
+}
+
+function matchName(array, name) {
+  return array.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
