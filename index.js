f564403773f9d7cd558b3a1a458813e8ca88fd5a
Ms-Alex <ms.hernandeza1@gmail.com>
2018-07-15 22:43:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+
+function findMatching(arr, name) {
+  return arr.filter(function (e) {
+    return e.toLowerCase() === name.toLowerCase();
+  })
+};
+
+function fuzzyMatch(arr, letters) {
+  return arr.filter(function (e) {
+    return e.toLowerCase().startsWith(letters.toLowerCase());
+  })
+};
+
+function matchName(obj, name) {
+  return obj.filter(function (e) {
+    return e.name.toLowerCase() === name.toLowerCase();
+  })
+}
