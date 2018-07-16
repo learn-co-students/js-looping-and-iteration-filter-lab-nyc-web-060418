761797fe6a5ba094b4d1dfff206bbd870c9f52a9
newmanbradm <newmanbradm@gmail.com>
2018-07-16 12:39:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+function findMatching(array, string) {
+  const newArray = array.filter(function(str) {return str.toLowerCase() === string.toLowerCase()});
+  return newArray;
+}
+function fuzzyMatch(array, string) {
+  const newArray = array.filter(function(str) {return str.startsWith(string)});
+  return newArray;
+}
+function matchName(array, string) {
+  const newArray = array.filter(function(obj) {return obj.name === string});
+  return newArray;
+}
